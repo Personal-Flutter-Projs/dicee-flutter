df6e89478d4493c2e3d8936abb3e19435f7af530
keturah <keturah@limestart.co>
2020-10-20 19:13:33 +0100

    first commit to github

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNum = 1;
+  int rightDiceNum = 2;
+
+  void changeDiceNum() {
+    setState(() {
+      leftDiceNum = Random().nextInt(6) + 1;
+      rightDiceNum = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+                child: Image.asset('images/dice$leftDiceNum.png'),
+                onPressed: () {
+                  changeDiceNum();
+                }),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeDiceNum();
+              },
+              child: Image.asset(
+                'images/dice$rightDiceNum.png',
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
